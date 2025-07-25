7f0407f7b038aa3ad0ee42387aeccca88653baca
Aryan Vinod <ayevinny@Aryans-MacBook-Air-4.local>
2025-07-25 13:11:34 -0700

    created prompts for llm to utilize

---
+++ prompts/gap_detection_prompt.py
@@ -0,0 +1,11 @@
+gap_prompt = """
+Check if the following question is addressed in the given documentation. If not, reply 'Not covered.'
+
+Documentation:
+{docs}
+
+Question:
+{question}
+
+Result:
+"""
